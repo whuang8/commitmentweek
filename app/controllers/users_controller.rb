1c49e36a2a4e1afcbe552d279fdb9501131211b7
whuang8 <huang554@purdue.edu>
2016-10-24 15:12:29 -0400

    Ceenter days

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,12 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find_by(uid: params[:id])
 		response = HTTParty.get "https://api.github.com/users/#{@user.username}/events/public"
-		
 		events = JSON.parse(response.body)
+		@commits = []
 		
 		events.each do |event|
 			if event["type"] == "PushEvent"
-				puts '----'
-				event["payload"]["commits"].each do |commit|
-					puts commit["message"]
-				end
+				@commits << event
 			end
 		end		
 	end
